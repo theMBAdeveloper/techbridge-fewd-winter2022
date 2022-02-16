4e1315638c10dd0ed8384dc1f02ef60348370f13
thembdadeveloper <thembadeveloper@gmail.com>
2022-02-15 20:23:03 -0600

    adding project phase2

---
+++ project-phase2/scripts.js
@@ -0,0 +1,46 @@
+//scripts for problems
+function resetFindInGrid() {
+    //set tbNumberToFind to blank
+    let tbNumberToFind = document.getElementById("tbNumberToFind");
+    tbNumberToFind.value = '';
+
+    //unhighlight spans
+    let spans = document.getElementsByTagName('span');
+    console.log("spans: ", spans);
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].classList.remove('spanHighlighted');
+    }
+}
+
+function resetFindInList() {
+     //set tbTextToFind to blank
+    let tbTextToFind = document.getElementById("tbTextToFind");
+    tbTextToFind.value = '';
+
+    //unhighlight spans
+    let spans = document.getElementsByTagName('span');
+    console.log("spans: ", spans);
+    for (let index = 0; index < spans.length; index++) {
+        spans[index].classList.remove('spanHighlighted');
+    }
+}
+
+
+
+function reloadPage() {
+    window.location.reload();
+    return false;
+}
+
+
+function findInGrid() {
+ //find in grid
+}
+
+function findInList() {
+   //find In List
+}
+
+function sortIt() {
+    //
+}
